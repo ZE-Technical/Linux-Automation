76a64b6f0be20cfca1ec5a7857a2c9da8b48e4c2
ZE-Technical <118942804+ZE-Technical@users.noreply.github.com>
2023-02-22 15:26:33 -0700

    Update terraform.sh

--- Ubuntu/terraform.sh
+++ Ubuntu/terraform.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download Terraform Binary from HashiCorp with wget
-wget https://releases.hashicorp.com/terraform/1.3.9/terraform_1.3.9_linux_amd64.zip
+wget -c https://releases.hashicorp.com/terraform/1.3.9/terraform_1.3.9_linux_amd64.zip
 
 # Unzip Terraform Binary
 unzip terraform_1.3.9_linux_amd64.zip
