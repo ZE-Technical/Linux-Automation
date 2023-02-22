bb263f58c9ef3535f52286af0f5740fc6a1105d7
ZE-Technical <118942804+ZE-Technical@users.noreply.github.com>
2023-02-22 14:06:58 -0700

    Create terrafrom.sh

---
+++ Ubuntu/terrafrom.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Download Terraform Binary from HashiCorp with wget
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+# Verify Terraform downloaded
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+# Install Terraform with apt
+apt-get update && apt-get install terraform
